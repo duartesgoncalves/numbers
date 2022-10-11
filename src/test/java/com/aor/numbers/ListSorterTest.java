d77c3871c66fbf1ead46445b692015604e902a61
Duarte Gonçalves <up202108772@edu.fe.up.pt>
2022-10-11 15:02:07 +0100

    Test setup simplified

--- src/test/java/com/aor/numbers/ListSorterTest.java
+++ src/test/java/com/aor/numbers/ListSorterTest.java
@@ -1,12 +1,20 @@
 package com.aor.numbers;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ListSorterTest {
+    private List<Integer> list, expected;
+
+    @BeforeEach
+    public void helper() {
+        list = Arrays.asList(3, 2, 6, 1, 4, 5, 7);
+        expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+    }
 
     @Test
     public void sort() {
